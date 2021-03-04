eac21328a6640962e95bcc89820dcc3e85aa922c
Artem Yurchenko <artem@studiovector.pl>
2021-03-04 14:40:15 +0100

    Weapon class and fire logic was implemented

---
+++ Assets/ArtemkaKun/Scripts/PlayerSystems/WeaponSystem/Weapon.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace ArtemkaKun.Scripts.PlayerSystems.WeaponSystem
+{
+    /// <summary>
+    /// Class, that maintains player's weapon.
+    /// </summary>
+    public sealed class Weapon : MonoBehaviour
+    {
+        [SerializeField] private WeaponData data;
+        [SerializeField] private Camera playerCamera;
+
+        private void Update()
+        {
+            if (!Google.XR.Cardboard.Api.IsTriggerPressed)
+            {
+                return;
+            }
+
+            if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out var hit, data.Range))
+            {
+                Destroy(hit.collider.gameObject); 
+            }
+        }
+    }
+}
---
+++ Assets/ArtemkaKun/Scripts/PlayerSystems/WeaponSystem/WeaponData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace ArtemkaKun.Scripts.PlayerSystems.WeaponSystem
+{
+    /// <summary>
+    /// Scriptable Object, that store player's weapon data.
+    /// </summary>
+    [CreateAssetMenu(fileName = "ScriptableObjects/WeaponData", menuName = "ScriptableObjects/WeaponData", order = 0)]
+    public sealed class WeaponData : ScriptableObject
+    {
+        public float Range => range;
+
+        [SerializeField] private float range;
+    }
+}
